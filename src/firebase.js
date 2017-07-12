87b1b8643f7bef6a6f2b47c1f1f5b34698074883
Bao Nguyen <baonguyen.work@gmail.com>
2017-07-12 17:51:20 +0700

    Done draft version of chatapp

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,8 @@
 import firebase from 'firebase';
 
+// Your firebase config
 var config = {
-  apiKey: "AIzaSyDQFLZzifjSAo-Z6VY2A-EO5BDj9_P1QY8",
-  authDomain: "chatapp-66650.firebaseapp.com",
-  databaseURL: "https://chatapp-66650.firebaseio.com",
-  projectId: "chatapp-66650",
-  storageBucket: "chatapp-66650.appspot.com",
-  messagingSenderId: "23160397680"
+
 };
 firebase.initializeApp(config);
 var a =1;
